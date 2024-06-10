004af414df21ba6949254c6f293e2f56a7c06015
Akhilesh023 <akhilesh.v2312@gmail.com>
2024-06-10 17:11:15 +0530

    warehouse and admin added

--- src/main/java/com/jsp/whms/controller/WareHouseController.java
+++ src/main/java/com/jsp/whms/controller/WareHouseController.java
@@ -2,10 +2,12 @@ package com.jsp.whms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jsp.whms.entity.WareHouse;
@@ -20,10 +22,12 @@ public class WareHouseController {
 	
 	@Autowired
 	private WareHouseService wareHouseService;
-	
-	@GetMapping("/warehouses")
-	public String addWareHouse(){
-		return "WareHouse Found";
+
+	@PreAuthorize("hasAuthority('CREATE_WAREHOUSE')") 
+	@PostMapping("/warehouses")
+	public ResponseEntity<ResponseStructure<WareHouseResponse>> addWareHouse(@RequestBody WareHouseRequest wareHouseRequest){
+		return wareHouseService.addWareHouse(wareHouseRequest);
 	}
 
+	
 }
--- src/main/java/com/jsp/whms/serviceimpli/WareHouseServiceImpli.java
+++ src/main/java/com/jsp/whms/serviceimpli/WareHouseServiceImpli.java
@@ -1,9 +1,14 @@
 package com.jsp.whms.serviceimpli;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.jsp.whms.entity.WareHouse;
+import com.jsp.whms.mapper.WareHouseMapper;
+import com.jsp.whms.repository.WareHouseRepository;
 import com.jsp.whms.requestdto.WareHouseRequest;
 import com.jsp.whms.responsedto.WareHouseResponse;
 import com.jsp.whms.service.WareHouseService;
@@ -11,14 +16,31 @@ import com.jsp.whms.util.ResponseStructure;
 
 @Service
 public class WareHouseServiceImpli implements WareHouseService {
+	
+	@Autowired
+	private WareHouseRepository wareHouseRepository;
+	 
+	@Autowired
+	private WareHouseMapper wareHouseMapper;
 
 	@Override
-	public ResponseEntity<ResponseStructure<WareHouseResponse>> addWareHouse() {
+	public ResponseEntity<ResponseStructure<WareHouseResponse>> addWareHouse(WareHouseRequest wareHouseRequest) {
+		
+		WareHouse wareHouse = wareHouseMapper.mapToWareHouse(wareHouseRequest, new WareHouse());
+		wareHouse = wareHouseRepository.save(wareHouse);
+		
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(new ResponseStructure<WareHouseResponse>()
+						.setStatus(HttpStatus.CREATED.value())
+						.setMessage("WareHouse Details are added")
+						.setData(wareHouseMapper.mapToWareHouseResponse(wareHouse)));
 		
 		
 		
+
 		
-		return null;
+
+		
 	}
 
 }
